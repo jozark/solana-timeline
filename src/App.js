1db48b8d8b567d14614191851c2ee94a921a31aa
Jozark <joshua.meyer.personal@gmail.com>
2022-10-17 13:20:16 +0200

    create intial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useEffect } from "react"
 
 function App() {
+  const checkIfWalletIsConnected = async () => {
+    // We're using optional chaining (question mark) to check if the object is null
+    if (window?.solana?.isPhantom) {
+      const response = await window.solana.connect({ onlyIfTrusted: true })
+      console.log("Connected with Public Key:", response.publicKey.toString())
+    } else {
+      alert("Solana object not found! Get a Phantom Wallet 👻")
+    }
+  }
+
+  const connectWallet = async () => {
+
+  }
+
+const renderNotConnectedContainer = () => (
+  <button onClick={connectWallet} className="cta-button connect-wallet-button">
+    Connect to Wallet
+  </button>
+)
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected()
+    }
+    window.addEventListener("load", onLoad)
+    return () => window.removeEventListener("load", onLoad)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🏛 Solana Historic Events</p>
+          <p className="sub-text">
+            A monument to save historic events happend in the solana.
+          </p>
+          {renderNotConnectedContainer()}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
